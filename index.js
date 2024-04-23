b24ef871fa8e8325f52859778ba32836cb62af13
Brandon Davis <wowzerss90@yahoo.com>
2024-04-23 02:55:10 -0400

    Created custom logger middleware. Moved data from index file into their own files. Created function to get a single member.

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
+const members = require("./Members");
+const logger = require("./middleware/logger");
 
-const members = [
-    {
-        id: 1,
-        name: "John Doe",
-        email: "john@gmail.com",
-        status: "active"
-    },
-    {
-        id: 2,
-        name: "Bob Williams",
-        email: "bob@gmail.com",
-        status: "inactive"
-    },
-    {
-        id: 3,
-        name: "Greg Davis",
-        email: "greg@gmail.com",
-        status: "active"
-    }
-]
-
+// Init middleware
+app.use(logger);
 
 app.get("/api/members", (req, res) => {
-    res.json(members);
+  res.json(members);
+});
+
+// Use + or parseInt() to convert params id to int
+app.get("/api/members/:id", (req, res) => {
+  const found = members.some((member) => member.id === +req.params.id);
+
+  if (found) {
+    res.json(members.filter((member) => member.id === +req.params.id));
+  } else {
+    res.status(400).json({ mgs: `No member with the id of ${req.params.id}` });
+  }
 });
 
 // Set static folder
@@ -35,5 +29,5 @@ app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
+  console.log(`Server started on port ${PORT}`);
 });
